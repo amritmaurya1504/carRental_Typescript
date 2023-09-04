4404e48be6fc6db3f2ba3e71a2243aab78cac00b
Amrit Raj <amrit.raj1504@gmail.com>
2023-09-05 02:47:48 +0530

    Car

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CarCard from "./CarCard";
+import SearchBar from "./SearchBar";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+import CustomFilter from "./CustomFilter";
+import SearchManufracturer from "./SearchManufracturer";
+
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufracturer, CarCard, CarDetails, ShowMore};
